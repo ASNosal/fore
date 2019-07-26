9c9b579403070145273c5f7104a625d98a5d49bb
Pat Ciambrone <pat@ciambr.one>
2019-07-26 13:17:05 -0400

    correct player header width calculation

--- fore.py
+++ fore.py
@@ -208,7 +208,7 @@ def print_table_data(jdata,tee_time_col,selected_players):
   w_player_col = 0
   for player in selected_players:
     if len(player) > w_player_col :
-      w_player_col = len(player) + 2
+      w_player_col = len(player) + 1
   w_pos_col = len("POS") + 2
   w_scr_col = len("TO PAR") + 3
   w_today_col = len("TODAY") + 3
@@ -220,7 +220,7 @@ def print_table_data(jdata,tee_time_col,selected_players):
   col_header = "| " + (' ' * int(((w_player_col - 6)/2))) + "PLAYER" + (' ' * int(((w_player_col - 6)/2))) + " | TEE TIME |"
   if(n_cols > 2):
     w_table = w_player_col + w_pos_col + w_scr_col + w_today_col + w_thru_col
-    col_header = "|  POS  | " + (' ' * int(((w_player_col - 6)/2))) + "PLAYER" + (' ' * int(((w_player_col - 6)/2))) + " |  TO PAR  |  TODAY  |   THRU    |"
+    col_header = "|  POS  |" + (' ' * int(((w_player_col - 6)/2))) + "PLAYER" + (' ' * round(((w_player_col - 6)/2))) + " |  TO PAR  |  TODAY  |   THRU    |"
 
   os.system('cls' if os.name == 'nt' else 'clear')
   player_print_cnt = 0
